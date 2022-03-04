a7260d3f13ea75b04b3e20cd29edbe42e2f0b16e
dycha0430 <68413811+dycha0430@users.noreply.github.com>
2022-03-05 03:32:12 +0900

    feat:gridview

--- app/src/main/java/com/example/unithon/HomeFragment.java
+++ app/src/main/java/com/example/unithon/HomeFragment.java
@@ -4,18 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.GridView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+    GridView gridView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
 
+        gridView = getActivity().findViewById(R.id.gridView);
 
 
         return rootView;
