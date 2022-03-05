822a080809f987eac784fba85353a75541c5aa76
rudolf0328 <ck0716080@gmail.com>
2022-03-06 01:56:38 +0900

    feat: comment

--- app/src/main/java/com/example/unithon/Comment/CommentActivity.java
+++ app/src/main/java/com/example/unithon/Comment/CommentActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.unithon.Model;
 import com.example.unithon.R;
@@ -15,11 +18,15 @@ public class CommentActivity extends AppCompatActivity {
     CommentAdapter commentAdapter;
     RecyclerView rcvComment;
     ArrayList<Model.Comment> commentArrayList = DummyComment.dummyCommentList;
+    ImageButton imgv_comment;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+        imgv_comment = (ImageButton) findViewById(R.id.imgb_add_comment);
+        editText = (EditText) findViewById(R.id.edt_comment);
 
         Intent intent = getIntent();
         int diary_num = intent.getIntExtra("diary_num", -1);
@@ -29,5 +36,13 @@ public class CommentActivity extends AppCompatActivity {
         commentAdapter = new CommentAdapter();
         rcvComment.setAdapter(commentAdapter);
         commentAdapter.submitList(commentArrayList);
+
+        imgv_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                commentArrayList.add(new Model.Comment("test user", "2022.03.06 1: 54", editText.getText().toString()));
+
+            }
+        });
     }
 }
