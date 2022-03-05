94aebc25016299440da07e47f278c05e5a4400b3
iamagirl99 <iamagirl99@naver.com>
2022-03-05 12:34:51 +0900

    font

---
+++ app/src/main/java/com/example/unithon/WriteDiaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.unithon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteDiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_diary);
+    }
+}
