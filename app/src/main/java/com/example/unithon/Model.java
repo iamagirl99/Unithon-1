429593af2ac117ed55a6345f31ec59cc95b90a4c
rudolf0328 <ck0716080@gmail.com>
2022-03-05 12:32:57 +0900

    feat: comment page

--- app/src/main/java/com/example/unithon/Model.java
+++ app/src/main/java/com/example/unithon/Model.java
@@ -4,4 +4,40 @@ public class Model {
     public class Diary {
         String name;
     }
+
+    public static class Comment {
+        private String writer, date, content;
+
+        public Comment() {}
+
+        public Comment(String writer, String date, String content) {
+            this.writer = writer;
+            this.date = date;
+            this.content = content;
+        }
+
+        public String getWriter() {
+            return writer;
+        }
+
+        public void setWriter(String writer) {
+            this.writer = writer;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+    }
 }
