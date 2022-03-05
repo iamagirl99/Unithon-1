5dc3bdf6da1e07ea35c6a272d88e050649f3c19c
rudolf0328 <ck0716080@gmail.com>
2022-03-05 15:59:43 +0900

    update changes

--- app/src/main/java/com/example/unithon/Home/EveryDiaryFragment.java
+++ app/src/main/java/com/example/unithon/Home/EveryDiaryFragment.java
@@ -16,7 +16,7 @@ public class EveryDiaryFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_others, container, false);
 
         return rootView;
     }
