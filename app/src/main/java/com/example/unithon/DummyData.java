4f1d4d1a1ff2ae6f051393ec4858f4ada6542f56
2019096144 <2019096144@hanyang.ac.kr>
2022-03-05 16:07:46 +0900

    cover recycler view

--- app/src/main/java/com/example/unithon/DummyData.java
+++ app/src/main/java/com/example/unithon/DummyData.java
@@ -7,6 +7,8 @@ public class DummyData {
     public static ArrayList<Integer> coverSrcList  = new ArrayList<Integer>() {
         {
             add(new Integer(R.drawable.cover_test));
+            add(new Integer(R.drawable.cover_test2));
+            add(new Integer(R.drawable.cover_test3));
         }
     };
 
